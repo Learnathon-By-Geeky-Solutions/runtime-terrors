e7bc273f359425c1349c4db1080beccc694752f4
hasin-ai <hasin-14-2021111354@it.du.ac.bd>
2025-01-22 12:17:51 +0600

    initial commit

---
+++ src/main/java/com/example/StockFlowAI/StockFlowAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.StockFlowAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockFlowAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockFlowAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StockFlowAI/StockFlowAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StockFlowAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockFlowAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
